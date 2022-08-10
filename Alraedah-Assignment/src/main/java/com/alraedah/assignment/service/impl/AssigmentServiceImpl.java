1ad391495ac301f3ff4a9462b3069a65e0e0c612
faisalmoin90 <faisalmoin90@gmail.com>
2022-08-10 19:30:17 +0400

    Update AssigmentServiceImpl.java

--- Alraedah-Assignment/src/main/java/com/alraedah/assignment/service/impl/AssigmentServiceImpl.java
+++ Alraedah-Assignment/src/main/java/com/alraedah/assignment/service/impl/AssigmentServiceImpl.java
@@ -1,10 +1,8 @@
 package com.alraedah.assignment.service.impl;
 
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
@@ -24,23 +22,29 @@ public class AssigmentServiceImpl implements AssigmentService {
 		log.info("Inside Count Cycle");
 		Map<String, Boolean> responseList = new LinkedHashMap<String, Boolean>();
 		for (Map.Entry<String, int[]> request : cycleDetectionRequestList.getInputList().entrySet()) {
-			Node head = null;
-			boolean checkLoop = true;
-			LinkedList<Node> linkedList = new LinkedList<>();
-			for (int i = request.getValue().length - 1; i >= 0; i--) {
-				head = new Node(request.getValue()[i], head);
-				linkedList.add(head);
-				if (request.getValue()[i] > request.getValue().length - 1) {
-					checkLoop = false;
+			if (request.getValue().length!=0) {
+				Node head = null;
+				boolean checkLoop = true;
+				LinkedList<Node> linkedList = new LinkedList<>();
+				for (int i = 0; i < request.getValue().length; i++) {
+					head = new Node(request.getValue()[i], null);
+					linkedList.add(head);
+					if (request.getValue()[i] > request.getValue().length - 1) {
+						checkLoop = false;
+					}
 				}
-			}
-			if (checkLoop) {
-				for (Node inputNode : linkedList) {
-					inputNode.next = linkedList.get(inputNode.data);
+				if (checkLoop) {
+					for (Node inputNode : linkedList) {
+						inputNode.next = linkedList.get(inputNode.data);
+					}
 				}
+
+				responseList.put(request.getKey(), checkLoop ? detectCycle(linkedList.getFirst()) : false);
+			} else {
+				// for Empty Array
+				responseList.put(request.getKey(), true);
 			}
 
-			responseList.put(request.getKey(), checkLoop ? detectCycle(head) : false);
 		}
 		CycleDetectionResponse cycleDetectionResponse = new CycleDetectionResponse();
 		cycleDetectionResponse.setResponseList(responseList);
@@ -49,16 +53,11 @@ public class AssigmentServiceImpl implements AssigmentService {
 
 	public static boolean detectCycle(Node head) {
 		Node curr = head;
-		Set<Node> set = new HashSet<>();
-		if (head == null) {
-			return true;
-		}
+		Node headNode = head;
 		while (curr != null) {
-			if (set.contains(curr)) {
+			if (curr.next == headNode) {
 				return true;
 			}
-
-			set.add(curr);
 			curr = curr.next;
 		}
 		return false;
